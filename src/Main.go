2566eba61630105bc95c2910226169d4223e35e7
James <jy1v07@gmail.com>
2016-01-21 21:27:27 +0000

    experimenting with go-lang strut and func

--- src/Main.go
+++ src/Main.go
@@ -1,4 +1,43 @@
 package main
 
+import "fmt"
+
 func main() {
+	em := Employee{"Emily", 26, female}
+	fmt.Println("Name: " , em.name, em.age, em.getSex())
+
+	bum := Business{"Emily's Business", nil}
+	fmt.Println(bum.getName())
 }
+
+type Employee struct {
+	name string
+	age int
+	sexNum Sex
+}
+
+type Sex int
+
+const (
+	male Sex = iota
+	female
+)
+
+var sexes = [...]string {
+	"male",
+	"female",
+}
+
+func (day Employee) getSex() string {
+	return sexes[day.sexNum]
+}
+
+type Business struct {
+	name string
+	employees []Employee
+}
+
+func (b Business) getName() (name string) {
+	return b.name
+}
+
