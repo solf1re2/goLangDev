f2b618ef3f5f60cd6c83168ec4dccfa1aaf96925
James <jy1v07@gmail.com>
2016-03-01 21:50:10 +0000

    Exercise 02 basics complete  - also repeats q on blank answer  TODO: GUI interface

--- 57Exercises/Exercise02.go
+++ 57Exercises/Exercise02.go
@@ -1,6 +1,28 @@
 package main
 
 import "fmt"
+import (
+	"log"
+)
 func main() {
+	var i = ""
+	fmt.Print("Hello, what is your name?")
+	i = getInput()
 
+	fmt.Println(i,"has" , len(i) , "characters")
+}
+
+func getInput() string {
+	var i string
+	_, err := fmt.Scanf("%s", &i)
+
+	if err != nil {
+		log.Print("  Input Error ", err)
+		i = ""
+	}
+	if len(i) == 0 {
+		fmt.Println("You must enter a name.")
+		return getInput()
+	}
+	return i
 }
