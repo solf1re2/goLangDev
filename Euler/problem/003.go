24f187051d698db6516e5f8f7881df69546de111
James <jy1v07@gmail.com>
2016-11-09 11:41:17 +0000

    some progress

--- Euler/problem/003.go
+++ Euler/problem/003.go
@@ -1,13 +1,47 @@
 package problem
 
+import (
+	"fmt"
+	"math"
+)
+
 /*
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 */
 
-func Three() {
-	for i := 0; i < 600851475143; i++ {
+// broken/locks up laptop
 
+var primeList []int
+var n int
+
+func Three() {
+	n = 600851475143
+	primeList = append(primeList, 1)
+	for i := 2; i < n; i++ {
+		//check if i is a factor of n
+		if n%i != 0 {
+			continue
+		}
+		// For each value of i - get the rounded up square root
+		roundedUpSqrt := int(math.Ceil(math.Sqrt(float64(i))))
+		// default bool to true, during checking it just needs to be disproved once to become false
+		iisPrime := true
+		// iterate over current slice of prime numbers
+		for j := range primeList[0:] {
+			fmt.Println(j)
+			fmt.Println(roundedUpSqrt)
+			if j != 0 {
+
+				if (roundedUpSqrt % j) == 0 {
+					iisPrime = false
+				}
+			}
+		}
+		if iisPrime {
+			primeList = append(primeList, i)
+		}
 	}
+	fmt.Println(primeList[len(primeList)])
 }
